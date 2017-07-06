1cbf036871d3cd2ceb1d71d2731388660d31190e
lijh <lijh@qcplay.com>
2017-07-06 16:56:37 +0800

    Initial commit

---
+++ CSDConverter/MainForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace CSDConverter
+{
+    partial class CSDConverter
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.srcPathLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dstPathLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // srcPathLabel
+            // 
+            this.srcPathLabel.AutoSize = true;
+            this.srcPathLabel.Location = new System.Drawing.Point(13, 13);
+            this.srcPathLabel.Name = "srcPathLabel";
+            this.srcPathLabel.Size = new System.Drawing.Size(71, 12);
+            this.srcPathLabel.TabIndex = 0;
+            this.srcPathLabel.Text = "源CSD路径：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(78, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(394, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(478, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(19, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dstPathLabel
+            // 
+            this.dstPathLabel.AutoSize = true;
+            this.dstPathLabel.Location = new System.Drawing.Point(13, 45);
+            this.dstPathLabel.Name = "dstPathLabel";
+            this.dstPathLabel.Size = new System.Drawing.Size(65, 12);
+            this.dstPathLabel.TabIndex = 4;
+            this.dstPathLabel.Text = "输出路径：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(78, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(394, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(19, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // CSDConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 269);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.dstPathLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.srcPathLabel);
+            this.Name = "CSDConverter";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label srcPathLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label dstPathLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CSDConverter/Util.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSDConverter
+{
+    class Util
+    {
+        private static String configFileName = "LocalConfig.json";
+
+        public static void saveSrcPath(string srcPath)
+        {
+            EnsureLocalConfig();
+        }
+
+        private static String EnsureLocalConfig()
+        {
+            String appDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "CSDConverter");
+            if (!Directory.Exists(appDir))
+                Directory.CreateDirectory(appDir);
+
+            String configPath = Path.Combine(appDir, configFileName);
+            if (!File.Exists(configPath))
+                File.Create(configPath);
+
+            return configPath;
+        }
+    }
+}
